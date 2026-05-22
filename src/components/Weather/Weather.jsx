0b37978510f656f6c3cdf3c71ef4524c30715a35
ゴンザガ ウェイン <ict2504@tsukougi-ict.net>
2026-05-22 15:49:55 +0900

    0522

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -6,20 +6,49 @@ import {
 import "./weather.css";
 
 import WeatherLocation from "./WeatherLocation";
-import WeatherIcon from "./WeatherIcon";
-import WeatherTemps from "./WeatherTemps";
+import {
+    WeatherPrimaryIcon,
+    WeatherPrimaryTemps,
+    WeatherSecondaryStats,
+} from "./WeatherStats";
 
 export default function WeatherModule() {
     const { weather, loading, error } = useWeather();
-
     if (loading) return <p>Loading weather...</p>;
     if (error) return <p>Error: {error}</p>;
     if (!weather) return <p>No weather data</p>;
 
     const cur = CurrentWeather(weather);
+    console.log(cur)
     const day = DailyWeather(weather);
-    console.log("Current Weather Data: ", cur); //
-    console.log("Daily Weather Data:", day);
+
+    console.error(weather);
+
+    const stats = {
+        icon: {
+            weatherCode: cur.weatherCode,
+            isDay: true,
+            isAnimated: true,
+            isFill: true,
+        },
+        temp: {
+            curTemp: cur.temperature,
+            maxTemp: day.tempMax[0],
+            minTemp: day.tempMin[0]
+        },
+        humidity: {
+            type: "humidity",
+            label: "Humidity",
+            value: cur.humidity,
+            unit: "%",
+        },
+        wind: {
+            type: "wind",
+            label: "Wind",
+            value: cur.windSpeed,
+            unit: "m/s",
+        },
+    };
 
     return (
         <div className="weather">
@@ -28,14 +57,18 @@ export default function WeatherModule() {
             </div>
             <div className="weather__body">
                 <div className="weather__primary">
-                    <WeatherIcon weatherCode={cur.weatherCode} />
-                    <WeatherTemps
-                        currentTemp={cur.temperature}
-                        maxTemp={day.tempMax[0]}
-                        minTemp={day.tempMin[0]}
-                    />
+                    <WeatherPrimaryIcon {...stats.icon}/>
+                    <WeatherPrimaryTemps {...stats.temp}/>
+                </div>
+                <div className="weather__secondary">
+                    <WeatherSecondaryStats {...stats.humidity}/>
+                    <WeatherSecondaryStats {...stats.wind}/>
+                    <WeatherSecondaryStats />
                 </div>
                 <div className="weather__secondary"></div>
+            </div>
+            <div className="weather__footer">
+                <p>Last Updated:    </p>
             </div>
         </div>
     );
